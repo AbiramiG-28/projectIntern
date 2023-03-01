ddfdcdfcb2c4e367363ee40719043d7e055e7bf8
AbiramiG-28 <126647378+AbiramiG-28@users.noreply.github.com>
2023-03-01 12:21:36 +0530

    java program

---
+++ methods.java
@@ -0,0 +1,13 @@
+class Methodm
+{
+	public static void main(String[]args){
+		int num;
+		Main mysqrt=new Main();
+		System.out.println("the square value is "+mysqrt.square());
+	}
+
+	 public int square(int a){
+		 int result= Math.sqrt(a);
+		 return result;
+	 }
+}
